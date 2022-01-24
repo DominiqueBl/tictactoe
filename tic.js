5aec9ccad43c59610bda0036f2bc3b52d7bd6337
Your Name <you@example.com>
2022-01-24 16:08:52 +0100

    added resp

--- tic.js
+++ tic.js
@@ -33,7 +33,7 @@ function restartGame(){
     document.getElementById('button').classList.add('d-none');
 
     for (let i = 1; i < 8; i++) {
-        document.getElementById('line' + i).classList.add('d-none');
+        document.getElementById('line-' + i).classList.add('d-none');
     }
     for (let i = 0; i < 9; i++) {
         document.getElementById('circle-' + i).classList.add('d-none');
@@ -59,49 +59,49 @@ function checkForWin() {
     //first row
     if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {  //wenn feld 0,1&1,2,also die ersten drei Felder gleich sind(zB mit Crosses oder Circles)hat jmd gewonnen
         winner = fields[0];
-        document.getElementById('line1').style.transform = 'scaleX(1)';
+        document.getElementById('line-1').style.transform = 'scaleX(1)';
     }
 
     if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {  
         winner = fields[4];
-        document.getElementById('line2').style.transform = 'scaleX(1)';
+        document.getElementById('line-2').style.transform = 'scaleX(1)';
     }
 
     if (fields[7] == fields[8] && fields[8] == fields[9] && fields[7]) { 
         winner = fields[7];
-        document.getElementById('line3').style.transform = 'scaleX(1)';
+        document.getElementById('line-3').style.transform = 'scaleX(1)';
     }
 
     if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {  
         winner = fields[0];
-        document.getElementById('line4').style.transform = 'rotate(90deg) scaleX(1)';
+        document.getElementById('line-4').style.transform = 'rotate(90deg) scaleX(1)';
     }
 
     if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {  
         winner = fields[1];
-        document.getElementById('line5').style.transform = 'rotate(90deg) scaleX(1)';
+        document.getElementById('line-5').style.transform = 'rotate(90deg) scaleX(1)';
 
     }
 
     if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {  
         winner = fields[2];
-        document.getElementById('line6').style.transform = 'rotate(90deg) scaleX(1)';
+        document.getElementById('line-6').style.transform = 'rotate(90deg) scaleX(1)';
     }
 
     if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {  
         winner = fields[0];
-        document.getElementById('line7').style.transform = 'rotate(45deg) scaleX(1.2)';
+        document.getElementById('line-7').style.transform = 'rotate(45deg) scaleX(1.2)';
 
     }
 
     //if (fields[0] == fields[5] && fields[5] == fields[8] && fields[0]) { 
-      //  winner = fields[0];
+     //   winner = fields[0];
        // document.getElementById('line8').style.transform = 'rotate(-45deg) scaleX(1.2)';
     //}
 
     if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {  
         winner = fields[2];
-        document.getElementById('line8').style.transform = 'rotate(-45deg) scaleX(1.2)';
+        document.getElementById('line-8').style.transform = 'rotate(-45deg) scaleX(1.2)';
     }
 
     if (winner) {
